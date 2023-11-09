2acc9274bfe1e6b7072a5fd2746f1d16b0bb8b09
Jesse Anderson <jeryanders@gmail.com>
2023-11-09 12:45:02 -0500

    fix: pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_public_access_block" "code_metrics" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-resource "aws_s3_bucket_public_access_block" "code_metrics" {
+resource "aws_s3_bucket_public_access_block" "code_metrics_test" {
   bucket = aws_s3_bucket.code_metrics.id
 
   block_public_acls       = true
