474957fffc37d0f866ef67ab388d08db92acb955
Jesse Anderson <jeryanders@gmail.com>
2023-11-09 12:44:09 -0500

    fix: pipeline

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,19 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_s3_bucket_public_access_block" "code_metrics" {
+  bucket = aws_s3_bucket.code_metrics.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_s3_bucket_public_access_block" "code_metrics" {
+  bucket = aws_s3_bucket.code_metrics.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
